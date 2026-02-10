66d29048ed0963dc1b68053e6a03d8eeb481aab3
Shaurya Saxena <shauryasaxena836@gmail.com>
2026-02-10 23:48:20 +0530

    update the graph code

--- graph.cpp
+++ graph.cpp
@@ -26,9 +26,26 @@ class Graph{
             cout << "invalid edge" << endl;
             return ;
         }
-        node* newnode = new node()
+        node* newnode = new node(v);
+        newnode->next=adj[u];
+        adj[v]=newnode;
+
+        newnode = new node(u);
+        newnode->next=adj[v];
+        adj[v]=newnode;
     }
-}
+    void printGraph(){
+        for(int i=0; i<v; i++){
+            cout << i << ":" ;
+            node* temp = adj[i];
+            while(temp){
+                cout << temp->vertex << "->" ;
+                temp=temp->next;
+            }
+            cout << "NULL\n";
+        }
+    }
+};
 int main()
 {
     return 0;
