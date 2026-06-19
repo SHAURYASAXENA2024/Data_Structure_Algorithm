7f5727d96d504960a91f6c57eaf823320aab8cdc
Shaurya Saxena <shauryasaxena836@gmail.com>
2026-06-19 13:27:04 +0530

    Code of the all the function of trie

---
+++ trie.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class node {
+public:
+  node *links[26];
+  bool flag = false;
+
+  node() {
+    for (int i = 0; i < 26; i++) {
+      links[i] = NULL;
+    }
+  }
+
+  bool containKey(char ch) { return links[ch - 'a'] != NULL; }
+
+  void put(char ch, node *newNode) { links[ch - 'a'] = newNode; }
+
+  node *get(char ch) { return links[ch - 'a']; }
+
+  void setEnd() { flag = true; }
+
+  bool isEnd() { return flag; }
+};
+
+class Trie : public node {
+private:
+  node *root;
+
+public:
+  Trie() { root = new node(); }
+
+  void insert(string str) {
+    node *temp = root;
+
+    for (int i = 0; i < str.length(); i++) {
+      if (!temp->containKey(str[i])) {
+        temp->put(str[i], new node());
+      }
+      temp = temp->get(str[i]); // FIXED
+    }
+    temp->setEnd(); // FIXED
+  }
+
+  bool search(string word) {
+    node *temp = root;
+
+    for (int i = 0; i < word.length(); i++) {
+      if (!temp->containKey(word[i])) {
+        return false;
+      }
+      temp = temp->get(word[i]); // FIXED
+    }
+
+    return temp->isEnd();
+  }
+
+  bool startswith(string prefix) {
+    node *temp = root;
+
+    for (int i = 0; i < prefix.length(); i++) {
+      if (!temp->containKey(prefix[i])) {
+        return false;
+      }
+      temp = temp->get(prefix[i]);
+    }
+
+    return true;
+  }
+};
+
+int main() {
+  Trie trie;
+
+  trie.insert("apple");
+
+  cout << trie.search("apple") << endl;   // 1
+  cout << trie.search("app") << endl;     // 0
+  cout << trie.startswith("app") << endl; // 1
+
+  return 0;
+}
